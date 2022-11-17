5679f103756549f41164ca7df892ec55d9eeef7a
Marcel Siegwart <marcel.siegwart@gmx.ch>
2022-11-17 10:14:01 +0100

    SPI Test

--- midriverTest/main.cpp
+++ midriverTest/main.cpp
@@ -1,18 +1,27 @@
 #include <cstdio>
 #include <stdio.h>
+#include <unistd.h>
+#include <iostream>
+#include <string>
 #include "../midriver/SPIDriver.h"
 
 int main()
 {
-    unsigned char data[] = { 0x11  , 0x22, 0x33 };
+    unsigned char rdata[100] = { 0 };
+    unsigned char tdata[] = { 0x55  , 0xaa, 0xee };
 
-    midriver::SPIDriver driver(midriver::SPIModes::SPIMode3,500000, midriver::SPI8Bits, midriver::SPIDev0);
-    int result = driver.SPITransfer(0, 3, data,data);
+    midriver::SPIDriver driver(midriver::SPIModes::SPIMode0,500000, midriver::SPI8Bits, midriver::SPIDev0);
+    
    
     printf("Hallo aus %s!\n", "midriverTest");
     while (1)
     {
-        //usleep(1000000);
+        int n = 0;
+        std::cin >> n;
+        std::cout << "send";
+        driver.SPITransfer(0, 3, tdata, rdata);
+        printf("rx 0x%2x 0x%2x 0x%2x\n", rdata[0], rdata[1], rdata[2]);
+        
     }
     return 0;
 }
